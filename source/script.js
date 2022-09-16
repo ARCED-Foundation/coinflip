5b54ff3ee43f83f732563d765540df9af14b9ae0
Mehrab Ali <mehrab.ali@arced.foundation>
2022-09-16 19:55:13 +0600

    Block for one flip while in the screen

--- source/script.js
+++ source/script.js
@@ -1,6 +1,7 @@
 /* global setAnswer, fieldProperties, getPluginParameter, $ */
 var head = getPluginParameter('head') // Title for head result
 var tail = getPluginParameter('tail') // Title for tail result
+var currentAnswer = fieldProperties.CURRENT_ANSWER 
 
 /* Store all filp results */
 var metadata = getMetaData() // To store all the results in metadata
@@ -17,6 +18,7 @@ function sleep(time) {
 }
 
 
+
 /*-- Coin flip annimation --*/
 
 let coin = document.getElementById("coin");
@@ -24,27 +26,28 @@ let coin = document.getElementById("coin");
 
 coin.addEventListener("click", fnClick);
 
+var block = 0;
 
 function fnClick() {
     var flipResult = Math.random();
-    if (flipResult < 0.5) {
+    if (flipResult < 0.5 && block === 0) {
         coin.className = "flipHead";
         sleep(5000).then(() => {
             /* Store data */
             setMetaData(metadata + "|" + head)
             setAnswer(head)
             document.getElementById("result").innerHTML = head;
-            
+            ++block
         });
 
-    } else {
+    } else if (flipResult >= 0.5 && block === 0) {
         coin.className = "flipTail";
         sleep(5000).then(() => {
             /* Store data */
             setMetaData(metadata + "|" + tail)
             setAnswer(tail)
             document.getElementById("result").innerHTML = tail;
-            
+            ++block
         });
     }
 }
